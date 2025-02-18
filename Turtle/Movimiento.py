fd93f55f438f35c1c5c2c254e5a05217e44ece24
Danikileitor <focusyi@hotmail.com>
2025-02-18 13:58:52 +0100

    Movimiento

---
+++ Turtle/Movimiento.py
@@ -0,0 +1,19 @@
+import turtle
+
+s = turtle.Screen()
+t = turtle.Turtle()
+
+t.goto(100,100)
+t.goto(-100,100)
+'''t.goto(0,0)'''
+t.home()
+
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+
+turtle.done()
