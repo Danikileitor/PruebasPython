00ffc490da9d25acc500ec24c36eb8715155db22
Danikileitor <focusyi@hotmail.com>
2025-02-18 14:22:44 +0100

    Personalizando

---
+++ Turtle/Personalizando.py
@@ -0,0 +1,20 @@
+import turtle
+
+s = turtle.Screen()
+t = turtle.Turtle()
+
+s.bgcolor("blue")
+s.title("Proyecto 1")
+
+t.shapesize(3, 3, 3)
+t.fillcolor("yellow")
+t.fd(100)
+t.pencolor("white")
+t.fd(100)
+t.color("red", "green")
+t.rt(90)
+t.fd(100)
+t.pensize(5)
+t.fd(100)
+
+turtle.done()
